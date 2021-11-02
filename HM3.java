b79692b5174071e95a78d9b190cf8d0a6f64a65d
Nikhil <93237125+Kratosjava123@users.noreply.github.com>
2021-11-02 20:58:42 +0530

    Add files via upload

---
+++ HM3.java
@@ -0,0 +1,16 @@
+public class Main{
+	public static void main(String args[]){
+		Systems.out.println("Hello user -"0;
+		int admission_yr = 2001;
+		String course = "CSE";
+		int rollno = 27;
+		if(admission_yr >= 2019)
+		{
+			System.out.println("The user's unique id = "+ admission_yr + course + rollno);
+		}
+		else
+		{
+			System.out.println("The user's umique id =" + rollno + course +admission_yr);
+		}
+	}
+}
